50a66f0ad200d4b5e3f4ac265269c87e51038b32
Yazan Tarifi <yazantarifi989@gmail.com>
2024-01-26 22:38:02 +0300

    Version

--- src/utils/tapper.info.ts
+++ src/utils/tapper.info.ts
@@ -1,5 +1,5 @@
 export class TapperInfo {
-    public static CLI_VERSION = "0.0.6";
+    public static CLI_VERSION = "0.0.8";
     public static CLI_SUPPORTED_PLATFORMS = "Android";
     public static CLI_HOMEPAGE = "https://tapperapp.com"
     public static CLI_SOURCE_CODE = "https://github.com/tapper-app/tapper-core"
